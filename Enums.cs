84545dff51c034d35cb649702039a1c41e7c097e
tc <tc@DESKTOP-TJTNSM1>
2021-03-25 09:50:24 +0100

    1.0Stable

--- Enums.cs
+++ Enums.cs
@@ -13,18 +13,5 @@ namespace Hitman3Reworked
         public string zPlayerOffset = "+02AB4B18,0x20,0x224";
         public string hitmanProcessName = "hitman3.exe";
 
-
-        //from 1.1 version (test if it's better to overwr)
-        public string yPlayerOffset2 = "+02AB4B18,0x20,0xEC";
-        public string xPlayerOffset2 = "+02AB4B18,0x20,0xDC";
-        public string zPlayerOffset2 = "+02AB4B18,0x20,0xE4";
-        public string yPlayerOffset3 = "+02AB4B18,0x20,0xD4";
-        public string xPlayerOffset3 = "+02AB4B18,0x20,0xC4";
-        public string zPlayerOffset3 = "+02AB4B18,0x20,0xCC";
-
-        public string realplayerY = "+02ABA5E8,1F8,88,60,60";
-        public string realplayerZ = "+02ABA5E8,1F8,88,60,5C";
-        public string realplayerX = "+02ABA5E8,1F8,88,60,58";
-
     }
 }
